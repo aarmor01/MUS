883fe7b75877d748010e5f4eb303e82ca58f74c3
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-02-17 14:43:43 +0100

    ej01 update

--- Ejercicios/hoja3/playVol.py
+++ Ejercicios/hoja3/playVol.py
@@ -1,21 +1,28 @@
 # 1_numPy/playNumPyVol.py reproductor con control de volumen
 import pyaudio, kbhit
+from Ejercicios.hoja2.Ej04 import SRATE
 from scipy.io import wavfile # para manejo de wavs
 import numpy as np  # arrays    
 from format_tools import *
 
+SRATE = 44100
 
 # abrimos wav y recogemos frecMuestreo y array de datos
 #fs, data = wavfile.read('../0_basics/expousure.wav')
-SRATE, data = wavfile.read('ex1.wav')
+# SRATE, data = wavfile.read('ex1.wav')
+
+def osc(frec, dur, vol):
+    return vol * np.sin(2 * np.pi * np.arange(int(SRATE * dur)) * frec / SRATE)
+
+
+data = osc(22050, 10, 0.8)
 
 data = toFloat32(data)
 
-
-print("Sample rate ",SRATE)
-print("Sample format: ",data.dtype)
-print("Num channels: ",len(data.shape))
-print("Len ",data.shape[0])
+print("Sample rate ", SRATE)
+print("Sample format: ", data.dtype)
+print("Num channels: ", len(data.shape))
+print("Len ", data.shape[0])
 
 # arrancamos pyAudio
 p = pyaudio.PyAudio()
@@ -33,10 +40,10 @@ stream = p.open(format=p.get_format_from_width(getWidthData(data)),
 bloque = np.arange(CHUNK,dtype=data.dtype)
 numBloque = 0
 kb = kbhit.KBHit()
-c= ' '
+c = ' '
 
 vol = 1.0
-while c!= 'q': 
+while c != 'q': 
     # nuevo bloque
     bloque = data[ numBloque*CHUNK : numBloque*CHUNK+CHUNK ]    
 
