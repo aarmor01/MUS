fd7ddc830fbf333248b2fc619e6f97fa6dbebae0
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-02-24 15:51:26 +0100

    ej07 update

--- Ejercicios/hoja3/Ej07.py
+++ Ejercicios/hoja3/Ej07.py
@@ -8,10 +8,75 @@ import numpy as np  # arrays
 import matplotlib.pyplot as plt
 from format_tools import *
 
-# SRATE = 44100
+SRATE = 44100
 CHUNK = 2048
 STDFREC = 440
 MODFREC = 27.5
+
+class Osc:
+    def __init__(self, f = 0):
+        self.frec = f
+        self.index = 0
+
+    def changeFrec(self, frec):
+        self.frec = frec
+
+    def next(self):
+        chunkWave = np.arange(CHUNK, dtype = np.float32)
+
+        for i in range(len(chunkWave)):
+            chunkWave[i] = np.sin((self.index + i) * (2 * np.pi) * self.frec / SRATE) # asumimos time = 1
+
+        self.index += CHUNK
+
+        return chunkWave
+
+    def nextRingMod(self):
+        chunkWave = np.arange(CHUNK, dtype = np.float32)
+
+        for i in range(len(chunkWave)):
+            chunkWave[i] = (np.sin((self.index + i) * (2 * np.pi) * self.frec / SRATE) + 1)/2  # asumimos time = 1
+
+        self.index += CHUNK
+
+        return chunkWave
+
+    def nextSquare(self):
+        chunkWave = np.arange(CHUNK, dtype = np.float32)
+
+        for i in range(len(chunkWave)):
+            # chunkWave[i] = signal.square((self.index + i) * (2 * np.pi) * self.frec / SRATE)
+            chunkWave[i] = 1 if np.sin((self.index + i) * (2 * np.pi) * self.frec / SRATE) > 0 else -1
+
+        self.index += CHUNK
+
+        return chunkWave
+
+    def nextTriangle(self):
+        chunkWave = np.arange(CHUNK, dtype = np.float32)
+
+        for i in range(len(chunkWave)):
+            chunkWave[i] = (2 / np.pi) * np.arcsin(np.sin((self.index + i) * (2 * np.pi) * self.frec / SRATE))
+
+        self.index += CHUNK
+
+        return chunkWave
+
+    def nextSaw(self):
+        chunkWave = np.arange(CHUNK, dtype = np.float32)
+
+        for i in range(len(chunkWave)):
+            #chunkWave[i] = (2 / numpy.pi) * numpy.arctan(numpy.tan(wave[i] / SRATE * (time * 2 * numpy.pi) / 2))
+            chunkWave[i] = (2 / np.pi) * np.arctan(np.tan(((self.index + i) * (2 * np.pi) * self.frec / SRATE)/2))
+
+        self.index += CHUNK
+
+        return chunkWave
+
+# Modulador de Amplitud
+def modula(sample, modWave):
+    for i in range(len(sample)):
+        sample[i] *= modWave[i]
 
 # Muestra matplotlib
 def setGraphics(wave):
@@ -20,68 +85,57 @@ def setGraphics(wave):
 
     plt.plot(x, y)
 
-data, SRATE = sf.read('piano2.wav')
-
-data = toFloat32(data)
+bloque = np.arange(CHUNK, dtype = np.float32)
 
 # abrimos stream
 stream = sd.OutputStream(samplerate=SRATE, 
     blocksize=CHUNK,
-    channels=len(data.shape))
+    channels=len(bloque.shape))
 
 # arrancamos stream
 stream.start()
 
 # En data tenemos el wav completo, ahora procesamos por bloques (chunks)
 kb = kbhit.KBHit()
+osc = Osc(STDFREC)
+oscMod = Osc(MODFREC)
 numBloque = 0
 c = ' '
 
-vol = 1.0
-while c != 'q': 
-     # numero de samples a procesar: CHUNK si quedan y si no, los que queden
-    nSamples = min(CHUNK, data.shape[0] - (numBloque + 1) * CHUNK)
 
-    # nuevo bloque
-    bloque = data[numBloque * CHUNK : numBloque * CHUNK + nSamples]
-
-    maxValue = np.amax(np.abs(bloque))
-    maxVol = 1 / maxValue
-
-    bloque *= min(maxVol, vol)
-
-    stream.write(bloque)        
-    
-    if kb.kbhit():
-        c = kb.getch()
-        if (c=='f'): STDFREC = max(220, STDFREC - 0.5)
-        elif (c=='F'): STDFREC = min(880, STDFREC + 0.5)
-        if (c=='v'): vol = max(0,vol - 0.05)
-        elif (c=='V'): vol = vol + 0.05
-        print("Vol: ", min(maxVol, vol))
-
-    numBloque += 1
-
-setGraphics(bloque)
-plt.show()
-
+# Happy Birthday
 notas = "A.BC.D.EF.G.a.bc.d.ef.g."
-happyFrec = [220*(2**(i/12)) for i in range(24)]
+songFrec = [440*(2**(i/12)) for i in range(24)]
+songIndex = 0
 
-x = happyFrec[notas.index('G')]
-
-[('G', 0.5), ('G',0.5), ('A',1), ('G',1),
-('c',1),('B',2),
-('G', 0.5), ('G',0.5), ('A',1), ('G',1),
+part = [('G', 0.5), ('G',0.5), ('a',1), ('G',1),
+('c',1),('b',2),
+('G', 0.5), ('G',0.5), ('a',1), ('G',1),
 ('d',1), ('c',2),
 ('G', 0.5), ('G',0.5), ('g',1),('e',1),
-('c',1),('B',1), ('A',1),
+('c',1),('b',1), ('a',1),
 ('f', 0.5), ('f',0.5), ('e',1),('c',1),
 ('d',1),('c',2),]
 
-print(x)
 
+
+while c != 'q' and songIndex < len(part) - 1: 
+    # nuevo bloque 
+    bloque = osc.next() 
+
+    osc.changeFrec(songFrec[notas.index(part[songIndex][0])])
+    # lastTime = part[songIndex][1]
+
+    stream.write(bloque)        
+
+    
+    songIndex += 1
+    numBloque += 1
+
+
+# setGraphics(bloqueMod)
+setGraphics(bloque)
+plt.show()
 
 kb.set_normal_term()        
 stream.stop()
-exit()
