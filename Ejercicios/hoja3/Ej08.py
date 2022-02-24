f7878ee5f07810427a1eb70f46120aadfa7b60f6
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-02-24 20:04:30 +0100

    ej08

---
+++ Ejercicios/hoja3/Ej08.py
@@ -0,0 +1,56 @@
+import kbhit
+import sounddevice as sd
+import soundfile as sf
+import numpy as np  
+import matplotlib.pyplot as plt
+import math
+
+SRATE = 44100
+CHUNK = 1024
+STDFREC = 440
+
+# Muestra matplotlib
+def setGraphics(wave):
+    x = list(range(len(wave)))
+    y = [sample for sample in wave]
+
+    plt.plot(x, y)
+
+data, SRATE = sf.read('piano.wav')
+
+# abrimos stream
+stream = sd.OutputStream(samplerate=SRATE, 
+    blocksize=CHUNK,
+    channels=len(data.shape))
+
+# arrancamos stream
+stream.start()
+
+# En data tenemos el wav completo, ahora procesamos por bloques (chunks)
+kb = kbhit.KBHit()
+numBloque = int(math.ceil(len(data) / CHUNK))
+c = ' '
+
+while c != 'q': 
+      # numero de samples a procesar: CHUNK si quedan y si no, los que queden
+    nSamples = min(CHUNK, data.shape[0] - (numBloque + 1) * CHUNK)
+
+    # nuevo bloque
+    bloque = data[numBloque * CHUNK : numBloque * CHUNK + nSamples]
+
+    if len(bloque) == 0:
+        stream.write(np.float32(bloque))
+
+    numBloque += 1
+
+    if kb.kbhit():
+        c = kb.getch()
+        if (c=='f'): numBloque = 0
+
+
+# setGraphics(bloque)
+# plt.show()
+
+kb.set_normal_term()        
+stream.stop()
+exit()
