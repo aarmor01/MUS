ddf8031785cf4a66240eebcfc9e8fb365562f509
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-03-01 20:55:03 +0100

    ej09 nueva version

--- Ejercicios/hoja3/Ej09.py
+++ Ejercicios/hoja3/Ej09.py
@@ -11,24 +11,26 @@ CHUNK = 2048
 STDFREC = 440
 MODFREC = 27.5
 
-class Delay:
-    def __init__(self, dT):
-        self.delayTime = dT
-        self.buf = np.zeros(dT * SRATE)
-
-    def extractChunk(self):  
-        return self.buf[:CHUNK]
-
-    def processChunk(self, audioChunk):
-        self.buf = np.delete(self.buf, np.s_[:CHUNK], axis = 0)
-        self.buf = np.append(self.buf, audioChunk, axis = 0)
-
 # Muestra matplotlib
 def setGraphics(wave):
     x = list(range(len(wave)))
     y = [sample for sample in wave]
 
     plt.plot(x, y)
+
+class Delay:
+    def __init__(self, dT):
+        self.delayTime = dT
+        self.buf = np.zeros(dT * SRATE)
+
+    def processChunk(self, audioChunk):
+        outputChunk = self.buf[:CHUNK]
+        self.buf = np.append(self.buf, audioChunk, axis = 0)
+        self.buf = self.buf[CHUNK:] # np.delete(self.buf, np.s_[:CHUNK], axis = 0)
+
+        # print(len(self.buf))
+        return outputChunk
+
 
 data, SRATE = sf.read('piano.wav')
 
@@ -45,20 +47,17 @@ kb = kbhit.KBHit()
 numBloque = 0
 c = ' '
 
-delay = Delay(1)
+delay = Delay(3)
 
 vol = 1.0
 while c != 'q': 
-    print('.')
      # numero de samples a procesar: CHUNK si quedan y si no, los que queden
     nSamples = min(CHUNK, data.shape[0] - (numBloque + 1) * CHUNK)
 
     # nuevo bloque
     bloque = data[numBloque * CHUNK : numBloque * CHUNK + nSamples]
 
-    stream.write(np.float32(delay.extractChunk()))
-    
-    delay.processChunk(bloque)
+    stream.write(np.float32(delay.processChunk(bloque)))
 
     if kb.kbhit():
         c = kb.getch()
@@ -69,9 +68,6 @@ while c != 'q':
     #     print("Vol: ", min(maxVol, vol))
 
     numBloque += 1
-
-setGraphics(bloque)
-plt.show()
 
 kb.set_normal_term()        
 stream.stop()
