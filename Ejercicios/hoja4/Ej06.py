c9af5f431ac272d5f592324f4779127b95404097
SergioJAlfonso <sergialf@ucm.es>
2022-03-16 16:51:08 +0100

    ej06 terminado
    relajao

--- Ejercicios/hoja4/Ej06.py
+++ Ejercicios/hoja4/Ej06.py
@@ -7,72 +7,78 @@ CHANNELS = 1
 CHUNK = 2048
 FADE_TIME = 300
 
-# leemos wav en array numpy (data)
-# por defecto lee en formato dtype="float64". No hay problema para reproducción simple (hace conversiones internas)
-data, SRATE = sf.read('flauta.wav', dtype=np.float64)
 
 # 0,3980 sube
 # 1,045 baja
 class Sampler:
     # constructura de la clase
-    def __init__(self, sample, isLooping):
-        self.sample = np.copy(sample)
-        self.iniSample = int(0.7185 * SRATE)
-        self.endSample = int(1.1280 * SRATE)
-        self.looping = isLooping
-        # compute fade out curve
-        # linear fade in
-        fade_curve = np.linspace(0.0, 1.0, FADE_TIME)
-        # apply the curve
-        self.sample[self.iniSample : self.iniSample + (FADE_TIME)] =  self.sample[self.iniSample : self.iniSample + (FADE_TIME)] * fade_curve
-        # linear fade out
-        fade_curve = np.linspace(1.0, 0.0, FADE_TIME)
-         # apply the curve
-        self.sample[self.endSample - FADE_TIME : self.endSample] =  self.sample[self.endSample - FADE_TIME : self.endSample] * fade_curve
-        plt.plot(self.sample[self.iniSample:self.endSample])
-        plt.show()
-        self.actSample = 0
+    def __init__(self, sample, start, end, isLooping):
+        self.sampleAudio = np.copy(sample)
+        self.startLoop = start
+        self.endLoop = end
+        self.isLooping = isLooping
+        self.index = 0
+
     def nextChunk(self):
-        limite = 0
-        if(self.looping and self.actSample + CHUNK > self.endSample):
-            limite = self.endSample
-        else:
-            limite = CHUNK + self.actSample
-        
-        outChunk = self.sample[self.actSample : limite]
+        if not self.isLooping:
+            samples = CHUNK
+            if CHUNK > len(self.sampleAudio):
+                samples = samples - (CHUNK - len(self.sampleAudio))
 
-        print(len(outChunk))
-        if(self.looping and limite == self.endSample):
-            outChunk = np.append(outChunk, self.sample[self.iniSample : self.iniSample + CHUNK - len(outChunk)])
-            self.actSample = self.iniSample + CHUNK - len(outChunk)
-        else:
-            self.actSample += CHUNK 
-        
-        if(len(outChunk) < CHUNK):
-            outChunk = np.append(outChunk, np.zeros(CHUNK - len(outChunk), dtype=np.float32))
-        return outChunk
-    def setLooping(self, bool):
-        self.looping = bool
+            outChunk = self.sampleAudio[self.index : self.index + samples]
 
-sampler = Sampler(data, True)
+            if len(outChunk) < CHUNK:
+                outChunk = np.append(outChunk, np.zeros(CHUNK - len(outChunk), dtype=np.float64))
+            
+            self.index += samples
+            return outChunk
+
+        else:
+            end = 0
+            if self.index + CHUNK > self.endLoop: end = self.endLoop
+            else: end = self.index + CHUNK
+
+            outChunk = self.sampleAudio[self.index : end]
+
+            if end == self.endLoop:
+                self.index = self.startLoop + (CHUNK - len(outChunk))
+                outChunk = np.append(outChunk, self.sampleAudio[self.startLoop : self.index])
+            else:
+                self.index += CHUNK 
+
+            return outChunk
+        
+    def setLooping(self, loop):
+        self.isLooping = loop
+        if self.isLooping: 
+            if self.index >= len(self.sampleAudio): self.index = 0
+            else: self.index = self.startLoop
+        else: self.index = self.startLoop
+
+# leemos wav en array numpy (data)
+# por defecto lee en formato dtype="float64". No hay problema para reproducción simple (hace conversiones internas)
+data, SRATE = sf.read('flauta.wav', dtype=np.float64)
+
+sampler = Sampler(data,  23896, 44134, False)
 def callbackO(outdata, frames, time, status):
     outdata[:,0] = sampler.nextChunk()
 
-stream = sd.OutputStream(
-    samplerate = SRATE,            # frec muestreo 
-    blocksize  = CHUNK,            # tamaño del bloque (muy recomendable unificarlo en todo el programa)
-    channels   = 1,
+stream = sd.OutputStream(samplerate = SRATE,            # frec muestreo 
+    blocksize = CHUNK,                                  # tamaño del bloque (muy recomendable unificarlo en todo el programa)
+    channels = 1,
     dtype = np.float32,
-    callback=callbackO)  # num de canales
+    callback=callbackO)                                 # num de canales
 
 # arrancamos stream
 stream.start()
 
 kb = kbhit.KBHit()
-# bloqueamos ejecucion para recoger respuesta
-while True:
+c= ' '
+
+while c != 'q':
  if kb.kbhit():
         c = kb.getch()
-        if(c == 'q'): sampler.setLooping(False)
+        if c == 'l': sampler.setLooping(True)
+        elif c == 'n': sampler.setLooping(False)
 
 kb.set_normal_term()
